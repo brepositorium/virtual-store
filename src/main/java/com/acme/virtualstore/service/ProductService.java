4b01a3180436768364161a2ad97ac0b7b640767b
brepositorium <rcryptob@protonmail.ch>
2024-08-06 13:59:34 +0300

    VS-6 add throws exception to ProductService interface

--- src/main/java/com/acme/virtualstore/service/ProductService.java
+++ src/main/java/com/acme/virtualstore/service/ProductService.java
@@ -4,8 +4,8 @@ import com.acme.virtualstore.dto.ProductDTO;
 
 public interface ProductService {
 
-    ProductDTO getProduct(Long id);
+    ProductDTO getProduct(Long id) throws Exception;
     ProductDTO createProduct(ProductDTO productDTO);
-    void deleteProduct(Long id);
+    void deleteProduct(Long id) throws Exception;
 
 }
