c3d0275a5921807733c510795f1aa00f603b1036
brepositorium <rcryptob@protonmail.ch>
2024-08-06 20:40:33 +0300

    VS-7 add product controller

---
+++ src/main/java/com/acme/virtualstore/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.acme.virtualstore.controller;
+
+import com.acme.virtualstore.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+
+
+}
