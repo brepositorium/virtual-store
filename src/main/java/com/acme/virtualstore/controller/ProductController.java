eadd6ab1152e160401f14a816dd6071cf0700352
brepositorium <rcryptob@protonmail.ch>
2024-08-06 20:41:18 +0300

    VS-7 add api to get product by id

--- src/main/java/com/acme/virtualstore/controller/ProductController.java
+++ src/main/java/com/acme/virtualstore/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.acme.virtualstore.controller;
 
+import com.acme.virtualstore.dto.ProductDTO;
 import com.acme.virtualstore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +17,10 @@ public class ProductController {
         this.productService = productService;
     }
 
-
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProduct(@PathVariable Long id) throws Exception {
+        ProductDTO productDTO = productService.getProduct(id);
+        return ResponseEntity.ok(productDTO);
+    }
 
 }
